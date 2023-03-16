3f2f308f4b0b7c1341c15a3a9e08810d694e8658
Iryna Shtynda <ira.shtynda@gmail.com>
2023-03-16 12:16:20 +0200

    added business, button, stats components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,7 @@ import styles from "../style";
 import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
 
-const Hero = () => {
-  return (
+const Hero = () => (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
@@ -42,6 +41,5 @@ const Hero = () => {
       </div>
     </section>
   );
-};
 
 export default Hero;
